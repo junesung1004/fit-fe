75f7a9f84a636c1c9a55c0b0f91516082612d24e
junesung1004 <junecoding1004@gmail.com>
2025-04-15 15:10:08 +0000

    Merge pull request #5 from fitdate/dev
    feat: 배포  테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 export default function LandingPage() {
   const router = useRouter();
 
+  // test
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push('/home');
