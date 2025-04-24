159521c1ad39ad23b84923bbabb9b34135c87e7d
junesung1004 <junecoding1004@gmail.com>
2025-04-24 11:16:21 +0000

    Merge pull request #57 from fitdate/dev
    feat: 배포용pr

--- src/app/(route)/home/HomeWrapper.tsx
+++ src/app/(route)/home/HomeWrapper.tsx
@@ -2,75 +2,46 @@
 
 import HomeFristProfileCardList from '@/components/page/home/HomeFirstProfileCardList';
 import HomeTwoProfileCardList from '@/components/page/home/HomeSecondProfileCardList';
-// import { DUMMYDATA } from '@/constants/homeDummyData';
 import {
   usePublicTodayDatingMatchMutation,
-  useTodayDatingMatchMutation,
-  // useTodayDatingMatchMutation,
 } from '@/hooks/mutation/useTodayDatingMatchMutation';
-import { useAuthStore } from '@/store/authStore';
 import { UserDataType } from '@/types/homePage.type';
 import React, { useEffect, useState } from 'react';
+
+interface MatchItem {
+  matchId: string;
+  user1: UserDataType;
+  user2: UserDataType;
+}
 
 export default function HomeWrapper() {
   const [firstUser, setFirstUser] = useState<UserDataType | null>(null);
   const [twoUser, setTwoUser] = useState<UserDataType | null>(null);
-  //console.log('firstUser :', firstUser);
   const [thirdUser, setThirdUser] = useState<UserDataType | null>(null);
   const [fourUser, setFourUser] = useState<UserDataType | null>(null);
-  const { mutate: todayDatingUser } = useTodayDatingMatchMutation();
+
   const { mutate: publicTodayDatingUser } = usePublicTodayDatingMatchMutation();
-  const [data, setData] = useState([]);
-  console.log('data :', data);
-  const { isLoggedIn } = useAuthStore();
-  const [publicData, setPublicData] = useState<{
-    matches: { user1: UserDataType; user2: UserDataType }[];
-  }>({
-    matches: [],
-  });
 
-  const getTodayDatingUserMatch = async () => {
-    todayDatingUser(undefined, {
-      onSuccess: (data) => {
-        // 여기서 data 배열을 설정
-        setData(data);
-      },
-      onError: (err) => {
-        console.error('❌ 매칭 데이터 가져오기 실패', err);
-      },
-    });
-  };
-
-  const getPublicTodayDatingUserMatch = async () => {
+  useEffect(() => {
     publicTodayDatingUser(undefined, {
-      onSuccess: (data) => {
-        // 여기서 data 배열을 설정
-        setPublicData(data);
+      onSuccess: (data: { matches: MatchItem[] }) => {
+        const matches = data.matches;
+        if (matches.length > 0) {
+          const { matchId, user1, user2 } = matches[0];
+          setFirstUser({ ...user1, matchId });
+          setTwoUser({  ...user2, matchId });
+        }
+        if (matches.length > 1) {
+          const { matchId, user1, user2 } = matches[1];
+          setThirdUser({ ...user1, matchId });
+          setFourUser({  ...user2, matchId });
+        }
       },
-      onError: (err) => {
+      onError: err => {
         console.error('❌ 매칭 데이터 가져오기 실패', err);
-      },
+      }
     });
-  };
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      getTodayDatingUserMatch(); // 🔐 로그인 유저용 API
-    } else {
-      getPublicTodayDatingUserMatch(); // 🌐 비로그인 유저용 API
-    }
-  }, [isLoggedIn]);
-
-  useEffect(() => {
-    if (publicData?.matches?.length > 0) {
-      setFirstUser(publicData.matches[0].user1);
-      setTwoUser(publicData.matches[0].user2);
-    }
-    if (publicData?.matches?.length > 0) {
-      setThirdUser(publicData.matches[1].user1);
-      setFourUser(publicData.matches[1].user2);
-    }
-  }, [publicData]);
+  }, []);
 
   return (
     <main className="p-3">
@@ -79,9 +50,14 @@ export default function HomeWrapper() {
         당신을 기다리는 인연이 도착합니다.
       </small>
 
-      {/* 메인 홈 프로필 카드 영역 */}
-      <HomeFristProfileCardList firstUser={firstUser} secondUser={twoUser} />
-      <HomeTwoProfileCardList thirdUser={thirdUser} fourUser={fourUser} />
+      <HomeFristProfileCardList
+        firstUser={firstUser}
+        secondUser={twoUser}
+      />
+      <HomeTwoProfileCardList
+        thirdUser={thirdUser}
+        fourUser={fourUser}
+      />
     </main>
   );
 }
