b9f957fae60ff7ba1e4890d54cf2e54a29df0293
junesung1004 <junecoding1004@gmail.com>
2025-04-24 14:00:00 +0000

    Merge pull request #59 from fitdate/dev
    feat: 배포용 pr

--- src/app/(route)/home/HomeWrapper.tsx
+++ src/app/(route)/home/HomeWrapper.tsx
@@ -71,6 +71,8 @@ export default function HomeWrapper() {
 
   // 로그인 여부에 따라 적절한 API 호출
   useEffect(() => {
+    if (typeof isLoggedIn !== 'boolean') return;
+
     if (isLoggedIn) {
       getTodayDatingUserMatch();
     } else {
