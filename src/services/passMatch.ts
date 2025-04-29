9de7f4fff97cbe885b8eefa7efbbb2e772b3a31a
junesung1004 <junecoding1004@gmail.com>
2025-04-29 06:32:29 +0000

    Merge pull request #125 from fitdate/dev
    feat: 배포용 pr

---
+++ src/services/passMatch.ts
@@ -0,0 +1,13 @@
+// src/services/passMatch.ts
+
+import instance from '@/lib/axios';
+
+export const passMatchRequest = async (passedUserId: string) => {
+  try {
+    const response = await instance.post(`/pass/match/${passedUserId}`);
+    return response.data; // 성공 시 서버 응답 반환
+  } catch (error) {
+    console.error('거절 요청 실패:', error);
+    throw error; // 호출한 쪽에서 에러 처리 가능하게 던짐
+  }
+};
