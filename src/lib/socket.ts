76beabce4cb25f4152fbcddae42c0416b1789dfa
junesung1004 <junecoding1004@gmail.com>
2025-04-30 07:43:36 +0000

    Merge pull request #157 from fitdate/feat/chat
    feat: 소켓 연결 옵션 추가

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,4 +4,6 @@ const SOCKET_URL = 'https://www.fit-date.co.kr';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
+  path: '/socket.io/',
+  transports: ['websocket', 'polling'],
 });
