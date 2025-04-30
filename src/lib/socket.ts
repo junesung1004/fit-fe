ecaa8a3db032b75333e8fde106af4ca361fd839c
junesung1004 <junecoding1004@gmail.com>
2025-04-30 07:32:46 +0000

    Merge pull request #156 from fitdate/feat/chat
    fix: socket url 경로 수정

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = 'https://fit-date.co.kr';
+const SOCKET_URL = 'https://www.fit-date.co.kr';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
