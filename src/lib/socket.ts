92352aebae530abfb06f1bc02ed3705e14ec3636
junesung1004 <junecoding1004@gmail.com>
2025-05-07 08:09:51 +0000

    Merge pull request #169 from fitdate/feat/chat
    Feat/chat

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,9 +1,10 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = 'https://www.fit-date.co.kr';
+const SOCKET_URL = 'https://api.fit-date.co.kr';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   path: '/socket.io/',
   transports: ['websocket'],
+  withCredentials: true,
 });
