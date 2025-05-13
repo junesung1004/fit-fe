94eb6f12339f546a0c54f7230462307611911826
junesung1004 <junecoding1004@gmail.com>
2025-05-13 06:40:36 +0000

    Merge pull request #255 from fitdate/feat/session
    fix: 소켓 path 경로 수정

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,7 +5,7 @@ const SOCKET_URL = 'https://api.fit-date.co.kr';
 // 채팅용 소켓
 export const chatSocket = io(SOCKET_URL, {
   autoConnect: false,
-  path: '/socket.io/chat',
+  path: '/socket.io/chat/',
   transports: ['websocket'],
   withCredentials: true,
 });
@@ -13,7 +13,7 @@ export const chatSocket = io(SOCKET_URL, {
 // 접속 상태용 소켓
 export const userStatusSocket = io(SOCKET_URL, {
   autoConnect: false,
-  path: '/socket.io/status',
+  path: '/socket.io/status/',
   transports: ['websocket'],
   withCredentials: true,
   reconnection: true,
